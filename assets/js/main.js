01703e0ab3ecbac9bdd268507a9fd578fe42f713
Aman <amanrajpoot5612@gmail.com>
2024-07-04 14:26:31 +0530

    updated form

--- assets/js/main.js
+++ assets/js/main.js
@@ -12,20 +12,20 @@
   "use strict";
 
 
-  document.getElementById('contact-form').addEventListener('submit', function(event) {
-    event.preventDefault();
+//   document.getElementById('contact-form').addEventListener('submit', function(event) {
+//     event.preventDefault();
 
-    const serviceID = 'service_eot472h';
-    const templateID = 'template_83erorv';
+//     const serviceID = 'service_eot472h';
+//     const templateID = 'template_83erorv';
 
-    emailjs.sendForm(serviceID, templateID, this)
-        .then(() => {
-            alert('Email sent successfully!');
-        }, (err) => {
-            alert('Failed to send email. Please try again later.');
-            console.log(JSON.stringify(err));
-        });
-});
+//     emailjs.sendForm(serviceID, templateID, this)
+//         .then(() => {
+//             alert('Email sent successfully!');
+//         }, (err) => {
+//             alert('Failed to send email. Please try again later.');
+//             console.log(JSON.stringify(err));
+//         });
+// });
   /**
    * Apply .scrolled class to the body as the page is scrolled down
    */
